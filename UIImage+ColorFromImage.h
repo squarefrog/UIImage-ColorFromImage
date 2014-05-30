f665efa66c4d48a49786e651ac3bf7350d0bbdaf
Paul Williamson <squarefrog@gmail.com>
2014-05-30 23:12:51 +0100

    Added classes

---
+++ UIImage+ColorFromImage.h
@@ -0,0 +1,19 @@
+//
+//  UIImage+ColorFromImage.h
+//  NUIDemo
+//
+//  Created by Paul Williamson on 30/05/2014.
+//  Copyright (c) 2014 Tom Benner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (ColorFromImage)
+
+/** Creates a 1x1 context, draws the image then returns a UIColor from the remaining pixel
+ 
+ @return The colour of the drawn pixel
+ */
+- (UIColor *)sqf_colorFromImage;
+
+@end
